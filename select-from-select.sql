6f2137c1b17e66ff6dc987a897ceea06dba3e2fe
jayk1p <joukyuu@hotmail.com>
2023-09-05 23:09:14 -0700

    Update select-from-select.sql

--- select-from-select.sql
+++ select-from-select.sql
@@ -1,6 +1,15 @@
 
-#6
+--#6
 
 SELECT name
 FROM world
-WHERE gdp > 4308854000000
+WHERE gdp > 4308854000000 /* This is the gdp of Germany, which has the highest GDP in europe */
+
+--OR
+
+SELECT name
+FROM world
+  WHERE gdp> 
+(SELECT gdp
+FROM world
+WHERE name = 'Germany')
